1dbcb4c49c4431a17d46ac45b4644ead2165f985
Khrongpop <mr.khrongpop@gmail.com>
2019-04-08 14:20:27 +0700

    Initial Commit

---
+++ BasicRx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasicRx
+//
+//  Created by Khrongpop on 8/4/2562 BE.
+//  Copyright © 2562 Khrongpop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
